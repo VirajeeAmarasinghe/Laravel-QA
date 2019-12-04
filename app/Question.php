262b50bb5cdf71d9cd59e73a2ae22f5efb474fef
Virajee <virajee.hiranthika@gmail.com>
2019-12-04 17:28:39 +0530

    Prepopulate users and questions tables using model factories and seed

--- app/Question.php
+++ app/Question.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Question extends Model
 {
-    protected $fillable=['title','body'];
+    protected $fillable=['title','slug','body','views','answers','votes','best_answer_id','user_id'];
 
     public function user(){
         return $this->belongsTo(User::class);
--- database/migrations/2019_12_03_152930_create_questions_table.php
+++ database/migrations/2019_12_03_152930_create_questions_table.php
@@ -15,7 +15,7 @@ class CreateQuestionsTable extends Migration
     {
         Schema::create('questions', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->string('title');
+            $table->string('title'); 
             $table->string('slug')->unique();
             $table->text('body');
             $table->unsignedInteger('views')->default(0);
